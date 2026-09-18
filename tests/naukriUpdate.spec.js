4202f09ad36c577d11e7b35266edd6004ca163d4
Tamilarasan R <ta06448@gmail.com>
2026-09-18 19:26:44 +0530

    linkedinpost pass file added

--- tests/naukriUpdate.spec.js
+++ tests/naukriUpdate.spec.js
@@ -5,7 +5,7 @@ test('Naukri Profile Update', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Enter Email ID / Username' }).click();
   await page.getByRole('textbox', { name: 'Enter Email ID / Username' }).fill('ta06448@gmail.com');
   await page.getByRole('textbox', { name: 'Enter Password' }).click();
-  await page.getByRole('textbox', { name: 'Enter Password' }).fill('Tamilsathya@0306');
+  await page.getByRole('textbox', { name: 'Enter Password' }).fill('Tamil');
   await page.getByText('Show', { exact: true }).click();
   await page.getByRole('button', { name: 'Login', exact: true }).click();
   await page.waitForTimeout(3000);
